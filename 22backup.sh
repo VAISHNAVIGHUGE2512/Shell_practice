ba3352af9beb9048e1d610aa122f30587f5813c9
VAISHNAVIGHUGE2512 <93418408+VAISHNAVIGHUGE2512@users.noreply.github.com>
2025-07-31 23:33:22 +0530

    Update 22backup.sh

--- 22backup.sh
+++ 22backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar cvf /tmp/backup.tar /etc /var
+tar -cvf /tmp/backup.tar /etc /var
 gzip /tmp/backup.tar
 find /tmp/backup.tar.gz -mtime -1 -type f -print &> /dev/null
 if [ $? -eq 0 ]
